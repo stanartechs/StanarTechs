123cdda49a3365db9c7069d3b53c687c714feb8d
cydbasilo <cydbasilo@gmail.com>
2025-09-28 18:53:34 -0700

    blog sort

--- js/blog-about.js
+++ js/blog-about.js
@@ -1,9 +1,14 @@
-const params = new URLSearchParams(window.location.search);
-const blogId = params.get('id');
+//const params = new URLSearchParams(window.location.search);
+//const blogId = params.get('id');
+function getBlogIdFromUrl() {
+    const params = new URLSearchParams(window.location.search);
+    return params.get('id');
+}
 
 fetch('blogs.json')
   .then(res => res.json())
   .then(blogs => {
+    const blogId = getBlogIdFromUrl();
     const blog = blogs.find(b => b.id == blogId);
     if (blog) {
       document.getElementById('blog-title').textContent = blog.title;
@@ -12,6 +17,16 @@ fetch('blogs.json')
       if (img) {
         img.src = blog.image;
         img.alt = blog.title;
+      }
+      // Insert content1, content2, content3, content4
+      const contentDiv = document.getElementById('blog-content');
+      const ps = contentDiv.querySelectorAll('p');
+      const blockquote = contentDiv.querySelector('blockquote.quote-post p');
+      if (ps.length >= 3 && blockquote) {
+          ps[0].textContent = blog.content1 || '';
+          blockquote.textContent = blog.content2 || '';
+          ps[1].textContent = blog.content3 || '';
+          ps[2].textContent = blog.content4 || '';
       }
     } else {
       document.getElementById('blog-title').textContent = 'Blog not found';
